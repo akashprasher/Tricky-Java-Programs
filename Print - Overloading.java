1b17d5632c83c9068de1c94c1ed3d4b6f697f7f7
Akash Prasher <prince.akash.prasher@gmail.com>
2020-07-07 20:28:52 +0530

    Create Print - Overloading.java

---
+++ Print - Overloading.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void print(String strArg) {
+        System.out.println("print(\"" + strArg + "\")");
+    }
+
+    public static void print(String strArg, int val) {
+        System.out.println("print(\"" + strArg + "\", " + val + ")");
+    }
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        int val = scanner.nextInt();
+        print(str);
+        print(str, val);
+    }
+}
